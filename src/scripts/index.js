3a4fd4889d2baa800a8acf4ab385ff67efb40bdb
Dmitry <d.av89@yandex.ru>
2020-07-21 20:09:58 +0300

    Закончил десктопную часть блока main

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,18 @@
-//import * as $ from 'jquery'
 import '../babel'
 import '../scss/style.scss'
 import {a} from './1.js'
 import Vue from 'vue'
+//LIBS AND FRAMEWORKS
 import MainBg from '../img/mainInformation/bg.jpg'
-import containerBig from '../components/containerBig.vue'
+import Ava from '../img/mainInformation/ava.jpg'
+import Vk1 from '../img/mainInformation/1.svg'
+import Inst1 from '../img/mainInformation/2.svg'
+import Github1 from '../img/mainInformation/3.svg'
+import Fb1 from '../img/mainInformation/4.svg'
+//IMAGES
+import containerBig from '../components/ContainerBig.vue'
+import Btn from '../components/Btn.vue'
+//COMPONENTS
 
 
 const app = new Vue({
@@ -13,6 +21,17 @@ const app = new Vue({
         message: 'Hi!!'
     },
     components: {
-      containerBig
+      containerBig,
+      Btn
+    },
+    methods: {
+      toggleMenu(){
+        let lines = document.querySelectorAll('.menu-btn__line'),
+            nav = document.querySelector('.navigation-block');
+        for(let i=0; i<lines.length; i++){
+          lines[i].classList.toggle('menu-btn_line_active');
+        }
+        nav.classList.toggle('navigation-block_active');
+      }
     }
 }) 
