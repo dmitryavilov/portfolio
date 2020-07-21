7fa0ea85f35746980785df98ebb40c44d5d220ad
Dmitry <d.av89@yandex.ru>
2020-07-21 12:16:06 +0300

    first commit

---
+++ src/components/containerBig.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container container_big">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+    .container {
+        max-width: 1034px;
+        margin: 0 auto;
+        width: 96%;
+    }
+
+    .container_big {
+        max-width: 1560px;
+    }
+</style>
---
+++ src/scripts/index.js
@@ -0,0 +1,17 @@
+//import * as $ from 'jquery'
+import '../babel'
+import '../scss/style.scss'
+import {a} from './1.js'
+import Vue from 'vue'
+import containerBig from '../components/containerBig.vue'
+
+
+const app = new Vue({
+    el: '.wrapper',
+    data: {
+        message: 'Hi!!'
+    },
+    components: {
+      containerBig
+    }
+}) 
