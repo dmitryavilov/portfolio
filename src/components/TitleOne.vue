847120ebb2cff82241ccce480f646411df524ea7
Dmitry <d.av89@yandex.ru>
2020-07-23 12:21:17 +0300

    Check

---
+++ src/components/TitleOne.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container container_big" :class="classes">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        classes: String
+    }
+}
+</script>
+
+<style lang="scss">
+    .container {
+        max-width: 1034px;
+        margin: 0 auto;
+        width: 96%;
+    }
+
+    .container_big {
+        max-width: 1560px;
+    }
+</style>
