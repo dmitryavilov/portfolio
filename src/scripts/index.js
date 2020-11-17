6f8b51338389b672905da0c71b51dbfad5a6ffea
Dmitry <d.av89@yandex.ru>
2020-11-17 15:37:13 +0300

    svg start

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,12 +9,15 @@ import Vk1 from '../img/mainInformation/1.svg'
 import Inst1 from '../img/mainInformation/2.svg'
 import Github1 from '../img/mainInformation/3.svg'
 import Fb1 from '../img/mainInformation/4.svg'
+import Sprite from '../img/sprite.svg'
 import About from '../img/about/about.png'
+import Keyboard from '../img/skills/keyboard.png'
 //IMAGES
 import Container from '../components/Container.vue'
 import ContainerBig from '../components/ContainerBig.vue'
 import Btn from '../components/Btn.vue'
 import TitleOne from '../components/TitleOne.vue'
+import TitleTwo from '../components/TitleTwo.vue'
 //COMPONENTS
 
 
@@ -27,7 +30,8 @@ const app = new Vue({
       Container,
       ContainerBig,
       Btn,
-      TitleOne
+      TitleOne,
+      TitleTwo
     },
     methods: {
       toggleMenu(){
