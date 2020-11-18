2d0cf7ecfee09609f5e84823fb23b7b8bf2796bc
Dmitry <d.av89@yandex.ru>
2020-11-18 15:50:47 +0300

    Закончил компьютерную версию блока skills

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,8 @@ import '../babel'
 import '../scss/style.scss'
 import {a} from './1.js'
 import Vue from 'vue'
+import VueEllipseProgress from 'vue-ellipse-progress'
+import WOW from 'wowjs'
 //LIBS AND FRAMEWORKS
 import MainBg from '../img/mainInformation/bg.jpg'
 import Ava from '../img/mainInformation/ava.jpg'
@@ -18,29 +20,55 @@ import ContainerBig from '../components/ContainerBig.vue'
 import Btn from '../components/Btn.vue'
 import TitleOne from '../components/TitleOne.vue'
 import TitleTwo from '../components/TitleTwo.vue'
-//COMPONENTS
+//COMPONENTs
 
-
+Vue.use(VueEllipseProgress);
 const app = new Vue({
-    el: '.wrapper',
-    data: {
-        about: About
-    },
-    components: {
-      Container,
-      ContainerBig,
-      Btn,
-      TitleOne,
-      TitleTwo
-    },
-    methods: {
-      toggleMenu(){
-        let lines = document.querySelectorAll('.menu-btn__line'),
-            nav = document.querySelector('.navigation-block');
-        for(let i=0; i<lines.length; i++){
-          lines[i].classList.toggle('menu-btn_line_active');
-        }
-        nav.classList.toggle('navigation-block_active');
+  el: '.wrapper',
+  data: {
+    about: About
+  },
+  components: {
+    Container,
+    ContainerBig,
+    Btn,
+    TitleOne,
+    TitleTwo
+  },
+  methods: {
+    toggleMenu() {
+      let lines = document.querySelectorAll('.menu-btn__line'),
+        nav = document.querySelector('.navigation-block');
+      for (let i = 0; i < lines.length; i++) {
+        lines[i].classList.toggle('menu-btn_line_active');
       }
+      nav.classList.toggle('navigation-block_active');
     }
-})
+  }
+});
+
+for ( let i of document.querySelectorAll(".skills-block__number") ) {
+
+  let numberTop = i.getBoundingClientRect().top,
+      start = +i.innerHTML,
+      end = +i.dataset.max;
+
+  window.addEventListener('scroll', function onScroll() {
+    if(window.pageYOffset > numberTop - window.innerHeight) {
+      this.removeEventListener('scroll', onScroll);
+      let interval = this.setInterval(function() {
+        i.innerHTML = ++start;
+        if(start == end) {
+          clearInterval(interval);
+        }
+    }, 9);
+    }
+  });
+}
+	
+
+var wow = new WOW.WOW({
+  live: false
+}).init();
+
+
